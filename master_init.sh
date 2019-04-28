d32f066ba71402f604ebff46c9f9cc620b6230a1
tqre <tkuure@gmail.com>
2019-04-28 16:36:35 +0300

    localhost master testing

--- master_init.sh
+++ master_init.sh
@@ -8,7 +8,7 @@ ssh-keygen
 cp /root/.ssh/id_rsa.pub /srv/salt/sshd/id_rsa.pub
 
 cat > /etc/salt/master << EOF
-interface: $(hostname -i)
+interface: 127.0.0.1
 EOF
 
 echo Master IP: $(hostname -i)
