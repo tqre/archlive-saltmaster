f0b3f178552ca7a1c22bc109cdeb310c8629c2a6
tqre <tkuure@gmail.com>
2019-05-08 16:41:53 +0300

    removed testing stuff

--- master_init.sh
+++ master_init.sh
@@ -8,24 +8,10 @@ ssh-keygen
 cp /root/.ssh/id_rsa.pub /srv/salt/sshd/id_rsa.pub
 
 cat > /etc/salt/master << EOF
-interface: 127.0.0.1
-EOF
-
-cat > /etc/salt/minion << EOF
-master: 127.0.0.1
-id: localminion
-
-virt:
-  connection:
-    uri: qemu:///system
-
+interface: 10.0.2.4
 EOF
 
 echo Master IP: $(hostname -i)
 
 echo Starting salt-master service...
 systemctl start salt-master
-
-#echo Starting local minion...
-#systemctl start salt-minion
-
