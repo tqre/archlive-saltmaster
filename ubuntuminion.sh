0070c555644af5b2dd84ae3b30a61c32f1b1e6a5
tqre <tkuure@gmail.com>
2019-05-12 19:45:48 +0300

    salting

--- ubuntuminion.sh
+++ ubuntuminion.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # This script makes a salt-minion out of Ubuntu-Server 19.04
-
+apt-get install salt-minion
+cat > /etc/salt/minion << EOF
+master: 10.0.2.4
+EOF
+systemctl restart salt-minion
